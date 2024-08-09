098361200395cde41b0525dc6b1b92d772af12e0
Vipul <vipulverma18260@gmail.com>
2024-08-10 01:29:14 +0530

    Nasa Mission Control

---
+++ Server/Src/App.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const planetsRouter = require('./Routes/Planets/planets.router');
+const cors=require('cors')
+
+const app=express()
+app.use(cors({
+    origin:'http://localhost:3000'
+}))
+app.use(express.json())
+app.use(planetsRouter)
+
+module.exports=app;
+
+
